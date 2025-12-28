a9a06e3da4525f95634436676d7a79861d56e85e
Manakal <manakal@randomsoftware.net>
2025-12-28 22:23:48 +0530

    Initial commit

---
+++ test/new_c_dk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NewCDk from '../lib/new_c_dk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/new_c_dk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NewCDk.NewCDkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
